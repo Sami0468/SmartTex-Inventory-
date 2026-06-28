c80e4853064dbbf37fb3c3d925e23422210cc69e
Muhammad Sami Rehman <samirehman0468@gmail.com>
2026-06-28 16:38:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -37,3 +37,9 @@ if __name__ == "__main__":
         db.create_all()
 
     app.run(debug=True, host="0.0.0.0", port=5000)
+    from app import create_app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
